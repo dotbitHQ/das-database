a187048a3b6f9083b571d2b9271ed29b734c98ad
jerry <i@sjis.me>
2023-02-22 17:03:13 +0800

    feat: reverse smt update

--- block_parser/block_parser.go
+++ block_parser/block_parser.go
@@ -247,7 +247,7 @@ var contractNames = []common.DasContractName{
 	common.DasContractNameIncomeCellType,
 	common.DasContractNameReverseRecordCellType,
 	common.DASContractNameEip712LibCellType,
-	common.DasContractNameReverseRecordRootCellType,
+	//common.DasContractNameReverseRecordRootCellType,
 }
 
 func (b *BlockParser) checkContractVersion() error {
