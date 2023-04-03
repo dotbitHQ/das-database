f5428ad069235d74c49e08ea6973dc208384a989
scorpiotzh <835598264@qq.com>
2023-04-03 11:06:35 +0800

    feat: Modify CheckContractVersion

--- block_parser/block_parser.go
+++ block_parser/block_parser.go
@@ -235,37 +235,3 @@ func (b *BlockParser) parserConcurrencyMode() error {
 	}
 	return nil
 }
-
-var contractNames = []common.DasContractName{
-	common.DasContractNameApplyRegisterCellType,
-	common.DasContractNamePreAccountCellType,
-	common.DasContractNameProposalCellType,
-	common.DasContractNameConfigCellType,
-	common.DasContractNameAccountCellType,
-	common.DasContractNameAccountSaleCellType,
-	common.DASContractNameSubAccountCellType,
-	common.DASContractNameOfferCellType,
-	common.DasContractNameBalanceCellType,
-	common.DasContractNameIncomeCellType,
-	common.DasContractNameReverseRecordCellType,
-	common.DASContractNameEip712LibCellType,
-	common.DasContractNameReverseRecordRootCellType,
-}
-
-func (b *BlockParser) checkContractVersion() error {
-	for _, v := range contractNames {
-		defaultVersion, chainVersion, err := b.dasCore.CheckContractVersion(v)
-		if err != nil {
-			if err == core.ErrContractMajorVersionDiff {
-				log.Errorf("contract[%s] version diff, chain[%s], service[%s].", v, chainVersion, defaultVersion)
-				log.Error("Please update the service. [https://github.com/dotbitHQ/das-database]")
-				if b.cancel != nil && !config.Cfg.Server.NotExit {
-					b.cancel()
-				}
-				return err
-			}
-			return fmt.Errorf("CheckContractVersion err: %s", err.Error())
-		}
-	}
-	return nil
-}
--- config/contract.go
+++ config/contract.go
@@ -20,6 +20,7 @@ var contractNames = []common.DasContractName{
 	//common.DasContractNameIncomeCellType,
 	common.DasContractNameReverseRecordCellType,
 	common.DASContractNameEip712LibCellType,
+	common.DasContractNameReverseRecordRootCellType,
 }
 
 func CheckContractVersion(dasCore *core.DasCore, cancel context.CancelFunc) error {
